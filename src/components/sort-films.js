1355ff087246bf1de3b7e818a18f238dbf398d05
bezprobeloff <44836223+bezprobeloff@users.noreply.github.com>
2020-01-20 14:01:14 +0300

    Начало добавления логики сортировки

--- src/components/sort-films.js
+++ src/components/sort-films.js
@@ -1,18 +1,50 @@
 import AbstractComponent from './abstract-component.js';
 
+export const SortType = {
+  DATE_DOWN: `date-down`,
+  RATE_DOWN: `rate-down`,
+  DEFAULT: `default`,
+};
+
 const createSortFilmsTemplate = () => {
   return (`
     <ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" data-sort-type="${SortType.DATE_DOWN}" class="sort__button">Sort by date</a></li>
+      <li><a href="#" data-sort-type="${SortType.RATE_DOWN}" class="sort__button">Sort by rating</a></li>
     </ul>
   `);
 };
 
 export default class SortFilm extends AbstractComponent {
 
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortFilmsTemplate();
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      this._currenSortType = sortType;
+
+      handler(this._currenSortType);
+    });
   }
 }
